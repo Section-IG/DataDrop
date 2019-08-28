13ac5eaf5ea10f7171488456120f5e33068de149
HunteRoi <tinael.devresse@hotmail.com>
2019-08-28 02:58:37 +0200

    Add email core

---
+++ commands/email.js
@@ -0,0 +1,13 @@
+exports.help = {
+    name: 'email',
+}
+
+module.exports = {
+    name: 'email',
+    description: 'Affiche une liste de mails selon les paramètres entrés',
+    usage: 'email [nom] | [flag]',
+
+    execute(client, message, args) {
+        
+    }
+}
