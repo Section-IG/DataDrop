792f54ae88e578a4574ca050a2969354ab32aef9
HunteRoi <tinael.devresse@hotmail.com>
2019-09-10 22:54:21 +0200

    Make the command asynchronous
    Making the execution asychronous in order to send the ok_hand before the process got killed.

--- commands/restart.js
+++ commands/restart.js
@@ -1,13 +1,13 @@
-const {ok_hand} = require('../config');
+﻿const {ok_hand} = require('../config');
 
 module.exports = {
 	name: 'restart',
     description: 'Relance le bot',
     ownerOnly: true,
 
-	execute(client, log, message, args) {
+    async execute(client, log, message, args) {
         log.info('Arrêt en cours...');
-        message.channel.send(ok_hand);
+        await message.channel.send(ok_hand);
         process.exit();
     }
 }
