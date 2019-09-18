977b1bee8c4b113fd94e46a7477ef73f44a9f9fc
HunteRoi <tinael.devresse@hotmail.com>
2019-09-19 00:34:42 +0200

    Refactor and add logging

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,6 +1,6 @@
 const { rolesChannelid,
-    ig1Roleid, ig2Roleid, ig3Roleid, alumniRoleid, tutorRoleid, announcementRoleid,
-    ig1Emote, ig2Emote, ig3Emote, alumniEmote, tutorEmote, announcementEmote } = require('../config');
+    ig1Roleid, ig2Roleid, ig3Roleid, alumniRoleid, tuteurRoleid, annoncesRoleid,
+    ig1Emote, ig2Emote, ig3Emote, alumniEmote, tuteurEmote, annoncesEmote } = require('../config');
 
 module.exports = async (client, log, messageReaction, user) => {
     const message = messageReaction.message;
@@ -12,30 +12,37 @@ module.exports = async (client, log, messageReaction, user) => {
     const ig2Role = message.guild.roles.get(ig2Roleid);
     const ig3Role = message.guild.roles.get(ig3Roleid);
     const alumniRole = message.guild.roles.get(alumniRoleid);
-    const tutorRole = message.guild.roles.get(tutorRoleid);
-    const announcementRole = message.guild.roles.get(announcementRoleid);
-    const emotes = [ig1Emote, ig2Emote, ig3Emote, alumniEmote, tutorEmote, announcementEmote];
+    const tuteurRole = message.guild.roles.get(tuteurRoleid);
+    const annoncesRole = message.guild.roles.get(annoncesRoleid);
+    const emotes = [ig1Emote, ig2Emote, ig3Emote, alumniEmote, tuteurEmote, annoncesEmote];
    
     if (emotes.includes(messageReaction.emoji.name) && message.channel.id === channel.id) {
         switch (messageReaction.emoji.name) {
             case ig1Emote:
                 member.removeRole(ig1Role).catch(log.error);
+                log.info(`Le rôle <${ig1Role.name}> a été retiré de <${member.user.tag}>`);
                 break;
             case ig2Emote:
                 member.removeRole(ig2Role).catch(log.error);
+                log.info(`Le rôle <${ig2Role.name}> a été retiré de <${member.user.tag}>`);
                 break;
             case ig3Emote:
                 member.removeRole(ig3Role).catch(log.error);
+                log.info(`Le rôle <${ig3Role.name}> a été retiré de <${member.user.tag}>`);
                 break;
             case alumniEmote:
                 member.removeRole(alumniRole).catch(log.error);
+                log.info(`Le rôle <${alumniRole.name}> a été retiré de <${member.user.tag}>`);
                 break;
-            case tutorEmote:
-                member.removeRole(tutorRole).catch(log.error);
+            case tuteurEmote:
+                member.removeRole(tuteurRole).catch(log.error);
+                log.info(`Le rôle <${tuteurRole.name}> a été retiré de <${member.user.tag}>`);
                 break;
-            case announcementEmote:
-                member.addRole(announcementRole).catch(log.error);
+            case annoncesEmote:
+                member.addRole(annoncesRole).catch(log.error);
+                log.info(`Le rôle <${annoncesRole.name}> a été ajouté à <${member.user.tag}>`);
                 break;
+                
             default:
                 break;
         }
