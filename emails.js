7c967dfe1659783f2f876a8dd6e5f334fb1f2f4c
tclaude94 <23203061+tclaude94@users.noreply.github.com>
2020-09-15 19:05:58 +0200

    Added few missing emails (#19)
    * Develop (#17)
    
    * Fix a lot of shit
    
    * Fix reference errors (#18)
    
    * Added few missing emails
    
    Co-authored-by: Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>

--- emails.js
+++ emails.js
@@ -326,9 +326,24 @@
             emails: ['martin.polson@henallux.be']
         },
         {
+            firstname: 'Nicolas',
+            lastname: 'Melchior',
+            emails: ['nicolas.melchior@henallux.be']
+        },
+        {
+            firstname: 'Secrétariat Étudiant',
+            lastname: 'Hénallux',
+            emails: ['secretu.iesn@henallux.be']
+        },
+        {
             firstname: 'Conseil Étudiant',
             lastname: 'Hénallux',
             emails: ['cehenallux@gmail.com']
+        },
+        {
+            firstname: 'Service Technique',
+            lastname: 'IESN',
+            emails: ['service.technique.campus.iesn@henallux.be']
         },
         {
             firstname: 'Accueil',
