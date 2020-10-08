d9f57066c123b38819969ee8d3180eb15f593ecc
HunteRoi <tinael.devresse@hotmail.com>
2020-10-08 03:27:08 +0200

    Fix a few issues and add "createtext" command
    Fix event "ready" filter for dynamicChannels, remove deleted voice channel from dynamicChannels collection

---
+++ commands/createtext.js
@@ -0,0 +1,37 @@
+const { dynamicChannelPrefix } = require('../config.js');
+
+module.exports = {
+  name: 'createtext',
+  description: 'Crée un canal texte temporaire lié au canal vocal temporaire et au créateur de ce dernier.',
+
+  async execute(client, log, message, args) {
+    const voiceChannel = message.member.voice.channel;
+
+    if (voiceChannel && client.dynamicChannels.has(voiceChannel.id)) {
+      const dChannelInfo = client.dynamicChannels.get(voiceChannel.id);
+      
+      if (dChannelInfo.authorId !== message.author.id) return;
+
+      log.info(`Le membre <${message.member.nickname}> (${message.member.id}) a lancé la création d'un canal écrit dynamique`);
+      
+      if (!dChannelInfo.textChannel) {
+        const newOptions = {
+          name: `${dynamicChannelPrefix}_${message.member.nickname}`,
+          parent: voiceChannel.parentID,
+          type: 'text',
+          permissionOverwrites: [{
+            id: message.member.id,
+            allow: ['MANAGE_CHANNELS'],
+            type: 'member'
+          }]
+        };
+        dChannelInfo.textChannel = await dChannelInfo.voiceChannel.clone(newOptions);
+      } 
+      else {
+        const response = 'Un canal écrit pour ce canal vocal existe déjà !';
+        log.info(response);
+        await message.channel.send(`:x: **Oups!** - ${response}`)
+      }
+    }
+  },
+};
