41b3064b425a12f918a6f798c60a683289763c6e
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2022-09-27 01:06:06 +0200

    refactor whole logic

--- src/commands/rolesreact.ts
+++ src/commands/rolesreact.ts
@@ -1,8 +1,13 @@
 import { Logger } from '@hunteroi/advanced-logger';
-import { EmbedBuilder, Message, Role, Snowflake, TextChannel } from 'discord.js';
-import { AnnounceConfiguration, SpecialRoleConfiguration, YearConfiguration } from '../models/Configuration';
+import { EmbedBuilder, Guild, Message, Snowflake, roleMention, Colors, ColorResolvable, TextBasedChannel, EmojiResolvable, formatEmoji } from 'discord.js';
 import { DatadropClient } from '../datadrop';
 import config from '../config';
+
+type RoleData = {
+  role: Snowflake;
+  emote: EmojiResolvable;
+  note?: string;
+};
 
 module.exports = {
   name: 'rolesreact',
@@ -10,71 +15,53 @@ module.exports = {
   ownerOnly: true,
 
   async execute(client: DatadropClient, log: Logger, message: Message, args: string[]) {
+    const { guild } = message;
+    if (!guild) return;
+
     const { rolesChannelid, ig1, ig2, ig3, alumni, tutor, driveManager, announce, ok_hand } = config;
 
-    await generateAndSendEmbed(
-      message,
-      rolesChannelid,
-      [ig1, ig2, ig3, alumni, tutor, driveManager, announce],
-      ['', '', '', '', '', '', '(note : retire le rôle quand la réaction est ajoutée)'],
-      'Les Professeurs, les Délégués, les Gestionnaires de Drive et les membres du Comité IG doivent notifier un Admin/Community Manager pour avoir leur rôle.'
-    );
-
-    await generateAndSendEmbed(message, ig1.channelid, ig1.groups);
-    await generateAndSendEmbed(message, ig2.channelid, ig2.groups);
-    await generateAndSendEmbed(message, ig3.channelid, ig3.groups);
+    await Promise.all([
+      generateAndSendEmbedToChannel(guild, rolesChannelid, [
+        { role: ig1.roleid, emote: ig1.emote },
+        { role: ig2.roleid, emote: ig2.emote },
+        { role: ig3.roleid, emote: ig3.emote },
+        { role: alumni.roleid, emote: alumni.emote },
+        { role: tutor.roleid, emote: tutor.emote },
+        { role: driveManager.roleid, emote: driveManager.emote },
+        { role: announce.roleid, emote: announce.emote, note: '(note : retire le rôle quand la réaction est ajoutée)' }
+      ], 'Les Professeurs, les Délégués, les Gestionnaires de Drive et les membres du Comité IG doivent notifier un Admin/Community Manager pour avoir leur rôle.'),
+      ...([ig1, ig2, ig3].map(roleData => generateAndSendEmbedToChannel(guild, roleData.channelid, roleData.groups.map(group => ({ role: group.roleid, emote: group.emote })))))
+    ]);
 
     return message.channel.send(ok_hand);
   },
 };
 
-async function generateAndSendEmbed(message: Message, channelId: Snowflake, roles: (YearConfiguration | SpecialRoleConfiguration | AnnounceConfiguration)[], notes: string[] = [], descriptionFooter = '') {
-  if (!message.guild) return;
-  if (!channelId) throw new Error('Channelid cannot be null');
-  if (!roles || !Array.isArray(roles) || roles.length === 0) throw new Error('Roles cannot be null or empty');
-  if (notes && !Array.isArray(notes)) throw new Error('The optional notes parameter must be an array');
+async function generateAndSendEmbedToChannel(guild: Guild, channelId: Snowflake, rolesData: RoleData[] = [], descriptionFooter = ''): Promise<void> {
+  if (rolesData.length === 0) throw new Error('Roles cannot be null or empty');
 
-  const channel = await message.guild.channels.fetch(channelId);
-  const guildRoles: Role[] = [];
-  for (let i = roles.length; i > roles.length; i--) { // reverse loop for order of display
-    const role = await message.guild.roles.fetch(roles[i].roleid);
-    if (role) guildRoles.push(role);
+  const channel = await guild.channels.fetch(channelId);
+  const descriptions = [];
+  for (const data of rolesData) {
+    const roleDescription = `${typeof data.emote === 'string' ? formatEmoji(data.emote) : data.emote} - ${roleMention(data.role)}${data.note ? ' ' + data.note : ''}`;
+    descriptions.push(roleDescription);
   }
 
-  const emotes = roles.map(r => r.emote);
-  const embed = generateEmbed(guildRoles, emotes, notes, descriptionFooter);
-  await sendEmbedToCorrespondingChannel(embed, channel as TextChannel, emotes);
+  const embed = toEmbed('Réagissez à ce message avec la réaction correspondante pour vous attribuer/retirer le rôle souhaité!', descriptions.join('\n'), descriptionFooter, Colors.DarkOrange);
+  const message = await (channel as TextBasedChannel).send({ embeds: [embed] });
+  for (const emote of rolesData.map(r => r.emote)) {
+    await message.react(emote);
+  }
 }
 
-function generateEmbed(roles: Role[], emotes: string[], notes: string[] = [], descriptionFooter = '') {
-  if (!roles) throw new Error('Roles cannot be null');
-  if (!emotes) throw new Error('Emotes cannot be null');
-  if (!Array.isArray(notes) && !descriptionFooter) {
-    descriptionFooter = notes;
-    notes = [];
-  }
-
-  let description = '';
-  for (let i = 0; i < roles.length; i++) {
-    description += `${emotes[i]} - ${roles[i].toString()}`;
-    if (notes[i]) description += ` ${notes[i]}`;
-    description += '\n';
-  }
-  description += `\n${descriptionFooter}`;
-
-  return new EmbedBuilder()
-    .setTitle('Réagissez à ce message avec la réaction correspondante pour vous attribuer/retirer le rôle souhaité!')
-    .setColor(0xdd9323)
+function toEmbed(title: string, description: string, footer: string, color: ColorResolvable): EmbedBuilder {
+  const embed = new EmbedBuilder()
+    .setTitle(title)
+    .setColor(color)
     .setDescription(description);
+
+  if (footer.length > 0) embed.setFooter({ text: footer });
+
+  return embed;
 }
 
-async function sendEmbedToCorrespondingChannel(embed: EmbedBuilder, channel: TextChannel, emotes: string[]) {
-  if (!embed) throw new Error('Embed cannot be null');
-  if (!channel) throw new Error('Channel cannot be null');
-  if (!emotes) throw new Error('Emotes cannot be null');
-
-  const message = await channel.send({ embeds: [embed] });
-  for (let i = 0; i < emotes.length; i++) {
-    await message.react(emotes[i]);
-  }
-}
