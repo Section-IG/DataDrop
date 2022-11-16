bc4f7973be0695976f3771f726d918f908aca850
HunteRoi <me@tinaeldevresse.eu>
2022-11-16 06:37:31 +0100

    feat: add link command
    It is also possible for old members to link their Discord account
    with their Henallux email by using this new command!

---
+++ src/commands/link.ts
@@ -0,0 +1,23 @@
+import { Logger } from '@hunteroi/advanced-logger';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Message } from 'discord.js';
+import { DatadropClient } from '../datadrop';
+
+module.exports = {
+    name: 'link',
+    description: 'Lie ton compte Discord avec ton adresse Hénallux!',
+
+    async execute(client: DatadropClient, log: Logger, message: Message, args: string[]) {
+        const user = message.author;
+        const linkAccountButton = new ButtonBuilder()
+            .setLabel('Lier son compte')
+            .setEmoji('🔗')
+            .setStyle(ButtonStyle.Primary)
+            .setCustomId(`lae${user.id}`);
+        const buttonComponent = new ActionRowBuilder<ButtonBuilder>().addComponents(linkAccountButton);
+
+        await message.channel.send({
+            content: "Pour lier son compte, rien de plus simple! Il faut cliquer sur le bouton ci-dessous et remplir le formulaire! Tu recevras un code par email qu'il faudra envoyer ici ensuite!",
+            components: [buttonComponent]
+        });
+    },
+};
